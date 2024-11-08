7a2c34b702a03800e9ee932bf1deb6d61aabf928
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-11-08 03:20:09 -0300

    atualizando

--- ex3/decrescente/contador_sincrono_decrescente_tb.v
+++ ex3/decrescente/contador_sincrono_decrescente_tb.v
@@ -5,31 +5,24 @@ module contador_sincrono_decrescente_tb;
     reg CK, CLR;
     wire [5:0] Q;
 
-    // Instancia o contador
     contador_sincrono_decrescente uut (CK, CLR, Q);
 
-    // Sinais de reset e teste
     initial begin
         $dumpfile("contador_sincrono_decrescente_tb.vcd"); 
         $dumpvars(0, contador_sincrono_decrescente_tb); 
 
-        CLR = 1;
-        #15 CLR = 0;            // Libera o reset após 15 unidades de tempo
+        CLR = 1; #15;
+        CLR = 0; #400;
+        CLR = 1; #10;
+        CLR = 0; #100;
 
-        // Testa a contagem por um tempo
-        #400;
-        CLR = 1;                // Aplica reset novamente
-        #10 CLR = 0;
-
-        #100;
         $display("Teste completo");                
         $finish;             
                
     end
 
-    // Geração de clock
     initial begin
         CK = 0;
-        forever #10 CK = ~CK;  // Clock com período de 20 unidades de tempo
+        forever #10 CK = ~CK;  
     end
 endmodule
